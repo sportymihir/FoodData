d2fce02df3dc29f8edb82e43f1ad7a8f3a2ac1c8
Mihir Mahale <mahalems1@s.dcsdk12.org>
2017-10-24 11:15:11 -0600

    Implement meal class

---
+++ meal.rb
@@ -0,0 +1,33 @@
+class Meal
+
+	attr_accessor :food, :name
+
+	def initialize(name)
+		@name = name
+		@food = []
+		@portion_sizes = []
+	end
+
+	def add_food(new_food, portion_size)
+		food.push(new_food)
+		@portion_sizes.push(portion_size)
+	end
+
+	def calories()
+		total_calories = 0
+		food.each do |food_item, i|
+			total_calories += food_item.total_calories(@portion_sizes[i])
+		end
+
+		total_calories
+	end
+
+	def to_s
+		str = name + "\n"
+		food.each_with_index do |food_item, i|
+			str += food_item.to_s + " | #{@portion_sizes[i]} \n"
+		end
+		str
+
+	end
+end
--- test.rb
+++ test.rb
@@ -1,5 +1,16 @@
-require "./food"
+require './food'
+require './meal'
 
 scrambled_eggs = Food.new("scrambled_eggs", "1 egg", 100)
+bacon = Food.new("bacon", "3 Slices", 100)
+cereal = Food.new("cereal", "1 oz", 135)
 
-puts scrambled_eggs
+#puts scrambled_eggs
+
+breakfast = Meal.new("breakfast")
+breakfast.add_food(scrambled_eggs, 3)
+breakfast.add_food(bacon, 2)
+breakfast.add_food(Food.new("cereal", "1 oz", 135), 2)
+
+puts breakfast
+puts "Breakfast Calories: #{breakfast.calories}"
